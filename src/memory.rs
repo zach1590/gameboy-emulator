1cfb3750e6dc5e412335f6cd0441c90008475327
zach1590 <zdsouza4@uwo.ca>
2022-01-04 22:22:30 -0500

    Create 'constructor' for Memory

--- src/memory.rs
+++ src/memory.rs
@@ -1,8 +1,19 @@
-struct Memory {
+pub struct Memory {
     mem: [u8; 65_536],              // 16 bit address = 64KiB of memory
     cart_mem: CartridgeMemory,      // Extra Memory Banks that may be needed 
 }
 
+impl Memory {
+    pub fn new() -> Memory {
+        return Memory {
+            mem: [0; 65_536],
+            cart_mem: CartridgeMemory {
+                rom_data_banks: Vec::new(),     // We will push on the u8 arrays once 
+                ram_data_banks: Vec::new(),     // we find out how many are required
+            },
+        }
+    }
+}
 
 // rom/ram_data are vectors of 8KiB arrays (Do rom/ram_size divided by 8192 to get the number of arrays)
 // rom/ram size will come from the cartridge header and get_rom/ram_size methods
