3609b4628f6e7cf97c883bea5903e48bd0bfd9c4
zach <zdsouza4@uwo.ca>
2022-07-29 19:27:19 -0400

    Fix joypad so that 0 is pressed/selected. Not 1

--- src/joypad.rs
+++ src/joypad.rs
@@ -26,7 +26,7 @@ impl Joypad {
     pub fn new() -> Joypad {
         return Joypad {
             event_pump: None,
-            joyp: 0x3F,
+            joyp: 0xCF,
             high_to_low: false,
             something_selected: false,
         };
@@ -91,32 +91,84 @@ impl Joypad {
             Keycode::D | Keycode::J => {
                 // Right or A (Bit 0)
                 self.high_to_low = true;
-                self.joyp = self.joyp & 0xF1;
+                self.joyp = (self.joyp | 0x0F) & 0xFE;
                 println!("Pressed Right or A");
             }
             Keycode::A | Keycode::K => {
                 // Left or B (Bit 1)
                 self.high_to_low = true;
-                self.joyp = self.joyp & 0xF2;
+                self.joyp = (self.joyp | 0x0F) & 0xFD;
                 println!("Pressed Left or B");
             }
             Keycode::W | Keycode::L => {
                 // Up or Select (Bit 2)
                 self.high_to_low = true;
-                self.joyp = self.joyp & 0xF4;
+                self.joyp = (self.joyp | 0x0F) & 0xFB;
                 println!("Pressed Up or Select");
             }
             Keycode::S | Keycode::H => {
                 // Down or Start (Bit 3)
                 self.high_to_low = true;
-                self.joyp = self.joyp & 0xF8;
+                self.joyp = (self.joyp | 0x0F) & 0xF7;
                 println!("Pressed Down or Start");
             }
             _ => {
                 // Nothing pressed
-                self.joyp = self.joyp & 0xF0;
+                self.joyp = self.joyp | 0x0F;
                 self.high_to_low = false;
             }
         }
     }
+}
+
+#[cfg(test)]
+#[test]
+pub fn test_handle_key_event() {
+    let mut joypad = Joypad::new();
+
+    joypad.joyp = 0xCF;
+    joypad.handle_key_event(Keycode::D);
+    assert_eq!(joypad.joyp, 0xCE);
+
+    joypad.handle_key_event(Keycode::K);
+    assert_eq!(joypad.joyp, 0xCD);
+
+    joypad.joyp = 0x2F;
+    joypad.handle_key_event(Keycode::L);
+    assert_eq!(joypad.joyp, 0x2B);
+
+    joypad.handle_key_event(Keycode::H);
+    assert_eq!(joypad.joyp, 0x27);
+}
+
+#[test]
+pub fn test_joypad_interrupt() {
+    let mut joypad = Joypad::new();
+
+    joypad.high_to_low = true;
+    joypad.write_byte(JOYP_REG, 0x27);
+    assert_eq!(true, joypad.is_joypad_interrupt());
+
+    joypad.write_byte(JOYP_REG, 0x3B);
+    assert_eq!(false, joypad.is_joypad_interrupt());
+
+    joypad.write_byte(JOYP_REG, 0x37);
+    assert_eq!(false, joypad.is_joypad_interrupt());
+
+    joypad.high_to_low = false;
+    joypad.write_byte(JOYP_REG, 0x1E);
+    assert_eq!(false, joypad.is_joypad_interrupt());
+
+    // Should the case where both selection inputs are selected
+    // return true or it should be useless entirely?
+    joypad.high_to_low = true;
+    joypad.write_byte(JOYP_REG, 0x0E);
+    assert_eq!(true, joypad.is_joypad_interrupt());
+
+    joypad.write_byte(JOYP_REG, 0x7E);
+    assert_eq!(false, joypad.is_joypad_interrupt());
+
+    joypad.high_to_low = false;
+    joypad.write_byte(JOYP_REG, 0x2E);
+    assert_eq!(false, joypad.is_joypad_interrupt());
 }
