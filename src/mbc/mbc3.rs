be98ec7f66aac211f7c1517f5d0690403ac8a587
zach <zdsouza4@uwo.ca>
2022-08-07 20:33:24 -0400

    Read and write to rom for mbc3
    Implemented read and write to rom for mbc 3 and have most of the
    struct variables decided for mbc3

--- src/mbc/mbc3.rs
+++ src/mbc/mbc3.rs
@@ -5,18 +5,21 @@ use super::battery::Battery;
 use super::mbc_timer::MbcTimer;
 use crate::mbc::Mbc;
 
+/*
+    Max 2MByte ROM  (128 Banks)
+    Max 32KByte RAM (4 Banks)
+
+    0x0000 - 0x3FFF Bank 0 non-swappable
+    0x4000 - 0x7FFF Bank 1 - 127 swappable
+*/
+
 pub struct Mbc3 {
-    rom: Vec<u8>, // bank 0 0x0000 - 0x3FFF(16384) and bank 1 0x4000 - 0x7FFF (bank1 is swappable)
-    ram: Vec<u8>, // 0xA000 - 0xBFFF
-    rom_offset: usize,
-    ram_offset: usize,
-    rom_bank: usize, // values of 0 and 1 both select bank 1 to be placed into 0x4000-0x7FFF
-    ram_bank: usize,
-    ext_bank: usize,
-    max_rom_banks: usize,
-    max_ram_banks: usize,
-    mode: u8,
-    ram_enabled: u8,
+    rom: Vec<u8>,                // 0x0000 - 0x7FFF
+    ram: Vec<u8>,                // 0xA000 - 0xBFFF
+    ram_and_timer_enable: u8,    // Value of 0xA will enable it
+    rom_bank_num: usize,         // 1 - 127 (0x01 - 0x7F)
+    ram_bank_num_and_rtc: usize, // 0 - 3
+    latch_data: u8,
     battery: Option<Battery>,
     timer: Option<MbcTimer>,
 }
@@ -26,15 +29,10 @@ impl Mbc3 {
         Mbc3 {
             rom: Vec::new(),
             ram: Vec::new(),
-            rom_offset: 0x4000,
-            ram_offset: 0,
-            rom_bank: 1,
-            ram_bank: 0,
-            ext_bank: 0,
-            max_rom_banks: 0x00,
-            max_ram_banks: 0x00,
-            mode: 0,
-            ram_enabled: 0x00,
+            ram_and_timer_enable: 0x00,
+            rom_bank_num: 0x01,
+            ram_bank_num_and_rtc: 0x00,
+            latch_data: 0x00,
             battery: None,
             timer: None,
         }
@@ -43,10 +41,22 @@ impl Mbc3 {
 
 impl Mbc for Mbc3 {
     fn read_rom_byte(self: &Self, addr: u16) -> u8 {
-        return 0xFF;
+        return match addr {
+            0x0000..=0x3FFF => self.rom[usize::from(addr)],
+            0x4000..=0x7FFF => self.rom[usize::from(addr) + (ROM_BANK_SIZE * self.rom_bank_num)],
+            _ => panic!("Invalid read from rom addr: {}", addr),
+        };
     }
 
-    fn write_rom_byte(self: &mut Self, addr: u16, val: u8) {}
+    fn write_rom_byte(self: &mut Self, addr: u16, val: u8) {
+        match addr {
+            0x0000..=0x1FFF => self.ram_and_timer_enable = val & 0x0A,
+            0x2000..=0x3FFF => self.rom_bank_num = if val == 0x00 { 0x01 } else { val as usize },
+            0x4000..=0x5FFF => self.ram_bank_num_and_rtc = val as usize,
+            0x6000..=0x7FFF => self.latch_data = val,
+            _ => panic!("Invalid write to rom addr: {}, with value: {}", addr, val),
+        }
+    }
 
     fn read_ram_byte(self: &Self, addr: u16) -> u8 {
         return 0xFF;
@@ -60,19 +70,19 @@ impl Mbc for Mbc3 {
         game_bytes: Vec<u8>,
         features: Vec<&str>,
         rom_size: usize,
-        rom_banks: usize,
+        _rom_banks: usize,
         ram_size: usize,
-        ram_banks: usize,
+        _ram_banks: usize,
     ) {
         self.rom = vec![0; rom_size];
-        self.max_rom_banks = rom_banks;
+        // self.max_rom_banks = rom_banks;
         self.rom = game_bytes;
 
         match features[..] {
             ["MBC3"] => { /* Nothing to do */ }
             ["MBC3", "RAM"] => {
                 self.ram = vec![0; ram_size];
-                self.max_ram_banks = ram_banks;
+                // self.max_ram_banks = ram_banks;
             }
             ["MBC3", "RAM", "BATTERY"] => {
                 let mut ram_path = String::from(game_path);
@@ -83,7 +93,7 @@ impl Mbc for Mbc3 {
 
                 self.ram = battery.load_ram();
                 self.battery = Some(battery);
-                self.max_ram_banks = ram_banks;
+                // self.max_ram_banks = ram_banks;
             }
             ["MBC3", "TIMER", "BATTERY"] => {}
             ["MBC3", "TIMER", "RAM", "BATTERY"] => {}
