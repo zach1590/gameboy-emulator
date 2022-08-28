ea5ff94d1bc7be8a84932387396d67b6ed7d221d
zach <zdsouza4@uwo.ca>
2022-08-28 00:50:44 -0400

    Better representation for channel 3

--- src/sound/channel3.rs
+++ src/sound/channel3.rs
@@ -1,52 +1,55 @@
+use super::Freq;
 #[allow(unused_imports)]
 use super::{NR30, NR31, NR32, NR33, NR34, WAVE_RAM_END, WAVE_RAM_START};
 
 pub struct Ch3 {
-    nr30: u8,
-    nr31: u8,
-    nr32: u8,
-    nr33: u8,
-    nr34: u8,
+    is_on: bool,      // NR30 (1 is playback)
+    len: u8,          // NR31
+    output_level: u8, // NR32 (Only bits 5-6 matter)
+    freq: Freq,       // NR33 and NR 34
 }
 
 impl Ch3 {
     pub fn new() -> Ch3 {
         Ch3 {
-            nr30: 0,
-            nr31: 0,
-            nr32: 0,
-            nr33: 0,
-            nr34: 0,
+            is_on: false,
+            len: 0,
+            output_level: 0,
+            freq: Freq::new(),
         }
     }
 
     pub fn read_byte(self: &Self, addr: u16) -> u8 {
         match addr {
-            NR30 => self.nr30,
-            NR31 => self.nr31 | 0xFF,
-            NR32 => self.nr32,
-            NR33 => self.nr33 | 0xFF,
-            NR34 => self.nr34 | 0x83,
+            NR30 => (self.is_on as u8) << 7 | 0x7F,
+            NR31 => self.len | 0xFF,
+            NR32 => (self.output_level << 5) | 0x9F,
+            NR33 => self.freq.get_lo(),
+            NR34 => self.freq.get_hi(),
             _ => panic!("ch3 does not handle reads from addr: {}", addr),
         }
     }
 
     pub fn write_byte(self: &mut Self, addr: u16, data: u8) {
         match addr {
-            NR30 => self.nr30 = data | 0x7F,
-            NR31 => self.nr31 = data,
-            NR32 => self.nr32 = data | 0x9F,
-            NR33 => self.nr33 = data,
-            NR34 => self.nr34 = data | 0x38,
+            NR30 => self.is_on = (data >> 7) & 0x01 == 0x01,
+            NR31 => self.len = data,
+            NR32 => self.output_level = (data >> 5) & 0x01,
+            NR33 => self.freq.set_lo(data),
+            NR34 => self.freq.set_hi(data),
             _ => panic!("ch3 does not handle writes to addr: {}", addr),
         }
     }
 
+    fn calc_len(self: &Self) -> f32 {
+        return ((256 - u16::from(self.len)) as f32) * (1. / 256.);
+    }
+
     pub fn dmg_init(self: &mut Self) {
-        self.nr30 = 0x7F;
-        self.nr31 = 0xFF;
-        self.nr32 = 0x9F;
-        self.nr33 = 0xFF;
-        self.nr34 = 0xBF;
+        self.is_on = false; // (0x7F >> 7) & 0x01 == 0x01
+        self.len = 0xFF;
+        self.output_level = 0x9F;
+        self.freq.set_lo(0xFF);
+        self.freq.set_hi(0xBF);
     }
 }
